70074acec68193646ec3085bfa399e5741992c45
Guilherme Melo <gui.melobarros10@hotmail.com>
2023-03-06 13:27:12 -0300

    Config Remote enable

--- hr-config-server/src/main/java/com/github/meloll/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/github/meloll/hrconfigserver/HrConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.github.meloll.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HrConfigServerApplication {
 
     public static void main(String[] args) {
