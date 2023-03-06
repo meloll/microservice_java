3c3139c36d7f31b2ad5e8a4e9eb599edea4a7ac8
Guilherme Melo <gui.melobarros10@hotmail.com>
2023-03-06 20:09:41 -0300

    Private GIT repository

--- hr-config-server/src/main/java/com/github/meloll/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/github/meloll/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,8 @@
 package com.github.meloll.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class HrConfigServerApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(HrConfigServerApplication.class, args);
