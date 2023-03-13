d2545c1847dea68f5e1f8213bd5e5257b5555703
Guilherme Melo <gui.melobarros10@hotmail.com>
2023-03-13 18:22:09 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/github/meloll/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.meloll.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
